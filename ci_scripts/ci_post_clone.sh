5b403054882851e21fea5b93896c1efd4e243cb4
derrickkim0109 <derrickkim0109@naver.com>
2025-07-05 21:45:18 +0900

    refactor: Tuist에 Xcode Cloud 수정

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
 set -e
 cd ..
-curl https://mise.run | sh
-export PATH="$HOME/.local/bin:$PATH"
-# Output the current PATH for debugging
-echo ":느낌표:️Current PATH: $PATH"
-echo ":느낌표:️mise version"
-mise --version
-echo ":느낌표:️mise install"
+curl https://mise.jdx.dev/install.sh | sh
 mise install # Installs the version from .mise.toml
-eval "$(mise activate bash --shims)"
-echo ":느낌표:️mise doctor"
-mise doctor # verify the output of mise is correct on CI
-echo ":느낌표:️tuist install"
-tuist install
-echo ":느낌표:️tuist generate"
-tuist generate # Generate the Xcode Project using Tuist
+# Runs the version of Tuist indicated in the .mise.toml file {#runs-the-version-of-tuist-indicated-in-the-misetoml-file}
+mise exec -- tuist generate
