db57308498446b391646db3e0532be46a9de5217
Greem <kim0517@hotmail.com>
2025-07-06 21:08:10 +0900

    [Feat] Xcode Cloud - GoogleService-Info.plist 연동 확인

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -14,6 +14,16 @@ echo "❗️mise install"
 mise install # Installs the version from .mise.toml
 eval "$(mise activate bash --shims)"
 
+
+echo "❗️ Setting GoogleService-Info.plist API_KEY : $API_KEY"
+
+echo "Project Directory: $PROJECT_DIR"
+
+plutil -replace API_KEY -string $API_KEY $PROJECT_DIR/App/Resources/GoogleService-Info.plist
+
+# GoogleService-Info.plist 파일 내용 출력
+plutil -p $PROJECT_DIR/App/Resources/GoogleService-Info.plist
+
 echo "❗️mise doctor"
 mise doctor # verify the output of mise is correct on CI
 echo "❗️tuist install"
--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -5,9 +5,6 @@ echo "Stage: PRE-Xcode Build Start"
 
 cd ..
 
-# API 환경변수 출력
-echo $API_KEY
-
 echo "Stage: PRE-Xcode Build End"
 
 exit 0
