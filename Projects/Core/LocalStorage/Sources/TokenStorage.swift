41c6ff2f540adcd73dad33ade54ca8f48bb8dd4d
derrickkim0109 <derrickkim0109@naver.com>
2025-07-09 22:42:17 +0900

    feat: TokenStorage 기능 추가

---
+++ Projects/Core/LocalStorage/Sources/TokenStorage.swift
@@ -0,0 +1,50 @@
+//
+//  TokenStorage.swift
+//  CoreLocalStorageInterface
+//
+//  Created by Derrick kim on 7/9/25.
+//
+
+import Foundation
+import CoreLocalStorageInterface
+import SharedUtil
+
+public struct TokenStorage: TokenStorageInterface {
+    public let keychain: Keychain
+
+    public init(keychain: Keychain) {
+        self.keychain = keychain
+    }
+
+    public init() {
+        self.keychain = .init()
+    }
+
+    public init(option: Keychain.Option) {
+        self.keychain = Keychain(option: option)
+    }
+
+    public func read<T: TokenType>(key: String) throws -> T? {
+        do {
+            guard let data = try keychain.read(key: key) else {
+                return nil
+            }
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            throw error
+        }
+    }
+
+    public func save<T: TokenType>(
+        token: T,
+        for key: String
+    ) throws {
+        let data = try JSONEncoder().encode(token)
+        try keychain.save(key: key, data: data)
+    }
+
+    @discardableResult
+    public func delete(for key: String) throws -> Bool {
+        return try keychain.delete(key: key)
+    }
+}
