0717769241cf01685ee66d883420528fd362aade
Greem <kim0517@hotmail.com>
2025-07-18 21:21:26 +0900

    [Feat] mise 설치 homebrew로 변경

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,32 +4,33 @@ set -e
 echo "❗️ Shell Script - Start Directory: $(pwd)"
 
 # mise 설치 (한 번만)
-curl https://mise.run | sh
+# curl https://mise.run | sh
+brew install mise
 
-# mise PATH 추가 (공식 문서 참고)
-# export PATH="$HOME/.local/share/mise/bin:$PATH"
+mise PATH 추가 (공식 문서 참고)
+export PATH="$HOME/.local/share/mise/bin:$PATH"
 
-# echo "❗️Current PATH: $PATH"
+echo "❗️Current PATH: $PATH"
 
-# # 하위 스크립트 실행
-# chmod +x ./ci_scripts/ci_post_clone_sub_scripts/app_settings.sh
-# ./ci_scripts/ci_post_clone_sub_scripts/app_settings.sh
-# chmod +x ./ci_scripts/ci_post_clone_sub_scripts/googleservice-info.sh
-# ./ci_scripts/ci_post_clone_sub_scripts/googleservice-info.sh
+# 하위 스크립트 실행
+chmod +x ./ci_scripts/ci_post_clone_sub_scripts/app_settings.sh
+./ci_scripts/ci_post_clone_sub_scripts/app_settings.sh
+chmod +x ./ci_scripts/ci_post_clone_sub_scripts/googleservice-info.sh
+./ci_scripts/ci_post_clone_sub_scripts/googleservice-info.sh
 
-# echo "❗️mise version"
-# mise --version
+echo "❗️mise version"
+mise --version
 
-# echo "❗️mise install"
-# mise install
+echo "❗️mise install"
+mise install
 
-# eval "$(mise activate sh --shims)"
+eval "$(mise activate sh --shims)"
 
-# echo "❗️mise doctor"
-# mise doctor
+echo "❗️mise doctor"
+mise doctor
 
-# echo "❗️tuist install"
-# tuist install
+echo "❗️tuist install"
+tuist install
 
-# echo "❗️tuist generate"
-# tuist generate
+echo "❗️tuist generate"
+tuist generate
