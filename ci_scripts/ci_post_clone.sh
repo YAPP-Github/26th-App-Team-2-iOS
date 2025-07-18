1a2eb239a49425f5f52867ab8621425171783826
Greem <kim0517@hotmail.com>
2025-07-18 21:43:46 +0900

    [Fix] 쉘 실행 위치 수정

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,10 +4,10 @@ set -e
 echo "❗️ Shell Script - Start Directory: $(pwd)"
 
 # 하위 스크립트 실행
-chmod +x ./ci_scripts/ci_post_clone_sub_scripts/app_settings.sh
-./ci_scripts/ci_post_clone_sub_scripts/app_settings.sh
-chmod +x ./ci_scripts/ci_post_clone_sub_scripts/googleservice-info.sh
-./ci_scripts/ci_post_clone_sub_scripts/googleservice-info.sh
+chmod +x ./ci_post_clone_sub_scripts/app_settings.sh
+./ci_post_clone_sub_scripts/app_settings.sh
+chmod +x ./ci_post_clone_sub_scripts/googleservice-info.sh
+./ci_post_clone_sub_scripts/googleservice-info.sh
 
 # mise 설치 (한 번만)
 # curl https://mise.run | sh
@@ -16,8 +16,6 @@ brew install mise
 export PATH="$HOME/.local/share/mise/bin:$PATH"
 
 echo "❗️Current PATH: $PATH"
-
-
 
 echo "❗️mise version"
 mise --version
