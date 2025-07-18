b20ecbe0c2976e23d50593ef4d0a06b2484dd2d1
Greem <kim0517@hotmail.com>
2025-07-18 21:40:46 +0900

    [Feat] 하위 shell script 바로 수정으로 위치 변경

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,6 +3,12 @@ set -e
 
 echo "❗️ Shell Script - Start Directory: $(pwd)"
 
+# 하위 스크립트 실행
+chmod +x ./ci_scripts/ci_post_clone_sub_scripts/app_settings.sh
+./ci_scripts/ci_post_clone_sub_scripts/app_settings.sh
+chmod +x ./ci_scripts/ci_post_clone_sub_scripts/googleservice-info.sh
+./ci_scripts/ci_post_clone_sub_scripts/googleservice-info.sh
+
 # mise 설치 (한 번만)
 # curl https://mise.run | sh
 brew install mise
@@ -11,11 +17,7 @@ export PATH="$HOME/.local/share/mise/bin:$PATH"
 
 echo "❗️Current PATH: $PATH"
 
-# 하위 스크립트 실행
-chmod +x ./ci_scripts/ci_post_clone_sub_scripts/app_settings.sh
-./ci_scripts/ci_post_clone_sub_scripts/app_settings.sh
-chmod +x ./ci_scripts/ci_post_clone_sub_scripts/googleservice-info.sh
-./ci_scripts/ci_post_clone_sub_scripts/googleservice-info.sh
+
 
 echo "❗️mise version"
 mise --version
