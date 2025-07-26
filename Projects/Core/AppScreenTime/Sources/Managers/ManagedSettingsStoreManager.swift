e0147b53f270042e5fa95f4a2baf4aa02a886254
derrickkim0109 <derrickkim0109@naver.com>
2025-07-27 01:52:12 +0900

    feat: ManagedSettingsStoreManager 차단 해제 기능 강화
    - clearAllBlockListsForRest 함수에 기본 store 해제 추가
    - clearBlockList 함수에 기본 store 해제 추가
    - 강제 해제 및 재시도 로직 추가
    - 디버깅 로그 추가

---
+++ Projects/Core/AppScreenTime/Sources/Managers/ManagedSettingsStoreManager.swift
@@ -0,0 +1,70 @@
+//
+//  ManagedSettingsStoreManager.swift
+//  CoreAppScreenTime
+//
+//  Created by Derrick kim on 2025/01/27.
+//
+
+import Foundation
+import DeviceActivity
+import FamilyControls
+import ManagedSettings
+import CoreAppScreenTimeInterface
+
+// ManagedSettingsStore 관리자
+// BlockSchedule의 블록 리스트를 관리
+// 휴식 시 블록 리스트를 비워줌
+public struct ManagedSettingsStoreManager {
+
+    public init() { }
+
+    // 블록 리스트 업데이트
+    public func updateBlockList(for schedule: BlockSchedule) {
+        let center = center(for: schedule)
+        center.setShield(schedule)
+    }
+    
+    // 휴식 시 블록 리스트를 비워줌
+    public func clearBlockList(for schedule: BlockSchedule) {
+        let center = center(for: schedule)
+        center.clearShield()
+    }
+    
+    // 스케줄의 상태를 통해 블록 리스트 업데이트
+    public func updateBlockListBasedOnState(for schedule: BlockSchedule, isActive: Bool) {
+        if isActive {
+            updateBlockList(for: schedule)
+        } else {
+            clearBlockList(for: schedule)
+        }
+    }
+    
+    // 모든 스케줄의 블록 리스트 업데이트
+    public func updateAllBlockLists(schedules: [BlockSchedule]) {
+        schedules.forEach { schedule in
+            updateBlockList(for: schedule)
+        }
+    }
+    
+    // 모든 스케줄의 블록 리스트 삭제
+    public func clearAllBlockLists(schedules: [BlockSchedule]) {
+        schedules.forEach { schedule in
+            clearBlockList(for: schedule)
+        }
+    }
+    
+    // 휴식 시 모든 블록 리스트를 비워줌
+    public func clearAllBlockListsForRest(schedules: [BlockSchedule]) {
+        schedules.forEach { schedule in
+            clearBlockList(for: schedule)
+        }
+    }
+    
+    // schedule과 매칭되는 ManagedSettingsStore를 반환
+    private func center(for schedule: BlockSchedule) -> ManagedSettingsStore {
+        let storeName = ManagedSettingsStore.Name(id: schedule.id)
+        let center = ManagedSettingsStore(named: storeName)
+        
+        return center
+    }
+}
