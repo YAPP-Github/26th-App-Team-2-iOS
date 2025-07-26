7b1459c4fbb9858935f3282d35da8327451e2421
derrickkim0109 <derrickkim0109@naver.com>
2025-07-27 01:52:48 +0900

    feat: 테스트 파일들 추가
    - BlockScheduleCalculateManagerTest 추가
    - BlockScheduleManagerTest 추가
    - ManagedSettingsStoreManagerTest 추가
    - AppScreenTimeTest 업데이트

--- Projects/Core/AppScreenTime/Tests/Sources/AppScreenTimeTest.swift
+++ Projects/Core/AppScreenTime/Tests/Sources/AppScreenTimeTest.swift
@@ -1,13 +1,18 @@
-//  AppScreenTime.swift
-//  Brake
+//
+//  AppScreenTimeTest.swift
+//  CoreAppScreenTime
 //
 //  Created by Derrick kim on 2025/07/12.
 //
 
+import Foundation
 import Testing
+import XCTest
 
 struct AppScreenTimeTest {
-    init() async throws { ... }
+    init() async throws {
+        
+    }
 
     @Test func myTest() {
 
