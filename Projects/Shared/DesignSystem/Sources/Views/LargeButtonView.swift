ae6a52beb182045541648bcfe5b67c587bda0183
derrickkim0109 <derrickkim0109@naver.com>
2025-07-27 20:46:34 +0900

    feat: LargeButton 컴포넌트 구현

---
+++ Projects/Shared/DesignSystem/Sources/Views/LargeButtonView.swift
@@ -0,0 +1,48 @@
+//
+//  LargeButtonView.swift
+//  SharedDesignSystem
+//
+//  Created by Derrick kim on 7/27/25.
+//
+
+import SwiftUI
+
+public struct LargeButtonView: View {
+
+    public let buttonTitle: String
+    public let isActive: Bool
+    public let height: CGFloat
+    public let action: () -> Void
+
+    public init(
+        buttonTitle: String,
+        isActive: Bool,
+        height: CGFloat = 56,
+        action: @escaping () -> Void
+    ) {
+        self.buttonTitle = buttonTitle
+        self.isActive = isActive
+        self.height = height
+        self.action = action
+    }
+
+    public var body: some View {
+        Button {
+            action()
+        } label: {
+            HStack {
+                Spacer()
+                Text(buttonTitle)
+                    .font(.pretendard(size: 16, type: .bold))
+                    .foregroundStyle(Colors.grey900.swiftUIColor)
+                Spacer()
+            }
+            .frame(height: height)
+            .frame(minWidth: 343, maxWidth: 408)
+            .background(isActive ? Colors.white.swiftUIColor : Colors.grey700.swiftUIColor)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+        }
+        .buttonStyle(.plain)
+        .interactiveDismissDisabled(true)
+    }
+}
