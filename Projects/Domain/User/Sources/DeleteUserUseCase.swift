c42f0506e7d235d3dd097fc2112a665257c249eb
derrickkim0109 <derrickkim0109@naver.com>
2025-08-02 23:56:49 +0900

    feat: 회원탈퇴 기능 구현
    - UserStorage에 닉네임 삭제 메소드 추가
    - TokenStorage에 모든 토큰 삭제 메소드 추가
    - UserProfileService에 토큰 삭제 기능 추가
    - DeleteUserUseCase 및 FetchUserNicknameUseCase 구현
    - 회원탈퇴 시 로컬 데이터 및 토큰 완전 삭제

---
+++ Projects/Domain/User/Sources/DeleteUserUseCase.swift
@@ -0,0 +1,25 @@
+//
+//  DeleteUserUseCase.swift
+//  DomainUser
+//
+//  Created by Derrick kim on 8/2/25.
+//
+
+import Core
+import DomainUserInterface
+
+public struct DeleteUserUseCase: DeleteUserUseCaseProtocol {
+
+    private let userProfileService: UserProfileProtocol
+
+    public init(
+        userProfileService: UserProfileProtocol
+    ) {
+        self.userProfileService = userProfileService
+    }
+
+    public func execute() async throws {
+        try await userProfileService.delete()
+    }
+
+}
