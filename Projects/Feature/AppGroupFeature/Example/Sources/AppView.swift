9674991bfd2ce2462fb4444eb281831945ce9510
Greem <kim0517@hotmail.com>
2025-08-02 21:32:37 +0900

    [Feat] 앱 메인 탭 뷰 컴포넌트 제작

--- Projects/Feature/AppGroupFeature/Example/Sources/AppView.swift
+++ Projects/Feature/AppGroupFeature/Example/Sources/AppView.swift
@@ -12,58 +12,60 @@ import SharedDesignSystem
 
 @main
 struct FeatureAppGroupFeatureApp: App {
-    
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    
     @Environment(\.appGroupDIContainer) var diContainer
     @State private var selectedTab: TabItemType = .dashboard
     var body: some Scene {
         WindowGroup {
-            BrakeTabView(selectedTab: $selectedTab)
-                .environment(
-                    AppGroupMainViewModel(
-                        fetchAppGroupUseCase: diContainer.fetchAppGroupUseCase,
-                        requestScreenTimeAuthUseCase: diContainer.requestScreenTimeAuthUseCase
-                    )
-                )
-                .safeAreaInset(edge: .bottom) {
-                    BrakeTabBarView(selectedTabBarItem: $selectedTab)
-                        .padding(.bottom, 16)
+            BrakeTabView(selectedTab: $selectedTab) { selectedTab in
+                ZStack {
+                    switch selectedTab {
+                    case .report:
+                        VStack {
+                            Spacer()
+                            Text("Report")
+                            Spacer()
+                        }
+                    case .dashboard:
+                        AppGroupMainView()
+                            .environment(
+                                AppGroupMainViewModel(
+                                    fetchAppGroupUseCase: diContainer.fetchAppGroupUseCase,
+                                    requestScreenTimeAuthUseCase: diContainer.requestScreenTimeAuthUseCase
+                                )
+                            )
+                    case .myInfo:
+                        VStack {
+                            Spacer()
+                            Text("My Info")
+                            Spacer()
+                        }
+                    }
                 }
+            }
         }
     }
 }
 
-struct BrakeTabView: View {
-    @Environment(\.appGroupDIContainer) var diContainer
-    @Environment(AppGroupMainViewModel.self) var appGroupViewModel
+struct BrakeTabView<Content: View>: View {
     @Binding var selectedTab: TabItemType
     @State private var bottomInsetHeight: CGFloat = 0
     
+    private let content: (TabItemType) -> Content
+    
+    init(selectedTab: Binding<TabItemType> ,content: @escaping (TabItemType) -> Content) {
+        self._selectedTab = selectedTab
+        self.content = content
+    }
+    
     var body: some View {
         GeometryReader { geometry in
-            ZStack {
-                switch selectedTab {
-                case .report:
-                    VStack {
-                        Spacer()
-                        Text("Report")
-                        Spacer()
-                    }
-                case .dashboard:
-                    AppGroupMainView()
-                        .environment(\.bottomInsetHeight, bottomInsetHeight)
-                case .myInfo:
-                    VStack {
-                        Spacer()
-                        Text("My Info")
-                        Spacer()
-                    }
-                }
-            }
-            .onAppear {
-                bottomInsetHeight = geometry.safeAreaInsets.bottom
-            }
+            self.content(selectedTab)
+                .environment(\.bottomInsetHeight, bottomInsetHeight)
+                .onAppear { bottomInsetHeight = geometry.safeAreaInsets.bottom }
+        }.safeAreaInset(edge: .bottom) {
+            BrakeTabBarView(selectedTabBarItem: $selectedTab)
+                .padding(.bottom, 16)
         }
     }
 }
