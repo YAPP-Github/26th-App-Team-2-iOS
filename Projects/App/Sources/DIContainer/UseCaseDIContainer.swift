71a498436d22fe193401ec39026fb3bb90239295
Greem <kim0517@hotmail.com>
2025-08-12 12:31:38 +0900

    [Chore] 브랜치 작업 기록

--- Projects/App/Sources/DIContainer/UseCaseDIContainer.swift
+++ Projects/App/Sources/DIContainer/UseCaseDIContainer.swift
@@ -83,9 +83,9 @@ public final class UseCaseDIContainer: UseCaseDIContainerProtocol {
     @MainActor public private(set) lazy var requestUserNotificationAuthUseCase: RequestUserNotificationAuthUseCase = RequestUserNotificationAuthUseCase()
     
     @MainActor public private(set) lazy var requestScreenTimeAuthUseCase: RequestScreenTimeAuthUseCase = RequestScreenTimeAuthUseCase()
-    public private(set) lazy var fetchUserNotificationAuthUseCase: FetchUserNotificationAuthUseCase = .init()
+    public private(set) lazy var fetchUserNotificationAuthUseCase: FetchUserNotificationAuthUseCase = FetchUserNotificationAuthUseCase()
     
-    public private(set) lazy var fetchScreenTimeAuthUseCase: FetchScreenTimeAuthUseCase = .init()
+    public private(set) lazy var fetchScreenTimeAuthUseCase: FetchScreenTimeAuthUseCase = FetchScreenTimeAuthUseCase()
     
     @MainActor public lazy var fetchAppGroupUseCase: FetchAppGroupUseCase = FetchAppGroupUseCase(
         appGroupService: serviceContainer.appGroupService
