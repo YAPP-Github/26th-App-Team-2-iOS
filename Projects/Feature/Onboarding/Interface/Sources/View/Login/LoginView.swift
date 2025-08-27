10745e23b1b388e4239d466374a8c000a452eb10
Greem <kim0517@hotmail.com>
2025-08-27 16:14:40 +0900

    [Refactor] 노티피케이션 요청 상태 변경

--- Projects/Feature/Onboarding/Interface/Sources/View/Login/LoginView.swift
+++ Projects/Feature/Onboarding/Interface/Sources/View/Login/LoginView.swift
@@ -58,9 +58,11 @@ public struct LoginView: View {
             content: {
                 NavigationView {
                     KakaoWebView { onSuccess in
+                        print("onSuccess: \(onSuccess)")
                         self.logInViewModel.kakaoCodeFetchSuccess(authorizationCode: onSuccess)
                         kakaoLogInShow.toggle()
                     } onAuthError: { error in
+                        print("onError: \(error)")
                         self.loading = false
                         kakaoLogInShow.toggle()
                     }
